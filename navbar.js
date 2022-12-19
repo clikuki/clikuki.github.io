3b1389227dfd71e8a96ce22025a0ddd9dc9d7a61
Clikuki <73450123+clikuki@users.noreply.github.com>
2022-12-19 13:23:35 +0800

    fixed navbar code and semantics

--- navbar.js
+++ navbar.js
@@ -1,4 +1,5 @@
-const navbarLinks = document.querySelector('.navbar .links .list');
+// If clicked link leads to current page, don't reload
+const navbarLinks = document.querySelector('header nav ul');
 if (navbarLinks) {
 	const curNavbarLink = navbarLinks.querySelector('[data-current-page]');
 	if (curNavbarLink) {
