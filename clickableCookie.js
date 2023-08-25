9dae9061c73abd70ff5f7c638b10ac81853f2692
Clikuki <73450123+clikuki@users.noreply.github.com>
2023-08-25 22:19:39 +0800

    - cookie bit rotation when dragged along edges - refactoring and stuff - removed dragging cursor - swapped setProperty with actual properties

--- clickableCookie.js
+++ clickableCookie.js
@@ -22,32 +22,18 @@ if (bigCookie) {
 	bigCookie.addEventListener('click', (e) => {
 		timeOfClick = Date.now();
 
-		const bigCookiePos = bigCookie.getBoundingClientRect();
-		const crack = {
-			opacity: 1,
-			img: document.createElement('img'),
-		};
-		const crackSize = 30;
-		crack.img.src = 'assets/crack.png';
-		crack.img.classList.add('cookieCrack');
-		crack.img.style.setProperty('--size', `${crackSize}px`);
-		crack.img.style.setProperty(
-			'--left',
-			`${e.pageX - crackSize / 2 - bigCookiePos.x}px`,
-		);
-		crack.img.style.setProperty(
-			'--top',
-			`${e.pageY - crackSize / 2 - bigCookiePos.y}px`,
-		);
-		crack.img.style.setProperty('--rotation', `${randomFloat(0, 359)}deg`);
+		// Crack
+		const crack = createCrack(e);
 		bigCookie.appendChild(crack.img);
 		cracks.push(crack);
 
+		// Physics cookie bits
 		const cookieBit = new CookieBit(e.pageX, e.pageY);
 		cookieContainer.appendChild(cookieBit.elem);
 		cookieBits.push(cookieBit);
 
-		// Create 2 splash bits at each of the 4 diagonals
+		// Small splash bits
+		// creates 2 bits at each diagonal
 		for (const x of [-1, 1]) {
 			for (const y of [-1, 1]) {
 				for (let i = 0; i < 2; i++) {
@@ -58,9 +44,10 @@ if (bigCookie) {
 			}
 		}
 
-		if (bigCookie.classList.contains('shake'))
+		if (bigCookie.classList.contains('shake')) {
 			bigCookie.classList.remove('shake');
-		setTimeout(() => bigCookie.classList.add('shake'));
+			setTimeout(() => bigCookie.classList.add('shake'));
+		} else bigCookie.classList.add('shake');
 	});
 
 	bigCookie.addEventListener('animationend', () => {
@@ -68,9 +55,6 @@ if (bigCookie) {
 	});
 
 	document.addEventListener('mouseup', () => {
-		if (draggedBit) {
-			draggedBit.elem.style.cursor = 'auto';
-		}
 		draggedBit = undefined;
 		document.body.style.userSelect = 'auto';
 		document.body.style.pointerEvents = 'all';
@@ -85,13 +69,20 @@ if (bigCookie) {
 			this.elem = document.createElement('div');
 			this.elem.classList.add('cookieBit');
 			this.opacity = 1;
-			this.size = randomFloat(30, 40);
-			this.pos = { x: x - this.size / 2, y: y - this.size / 2 };
-			this.vel =
-				Math.random() < 0.1 // 10% chance of a stronger velocity
-					? { x: randomFloat(-30, 30), y: randomFloat(0, -30) }
-					: { x: randomFloat(-5, 5), y: randomFloat(0, -10) };
+			this.size = randomFloat(30, 50);
 			this.rotation = randomFloat(0, 360);
+			this.pos = { x: x - this.size / 2, y: y - this.size / 2 };
+			const xDir = randomFloat(0, 10) <= 5 ? 1 : -1;
+			this.vel =
+				Math.random() < 0.1
+					? {
+							x: randomFloat(5, 20) * xDir,
+							y: randomFloat(-5, -20),
+					  }
+					: {
+							x: randomFloat(2, 10) * xDir,
+							y: randomFloat(-2, -10),
+					  };
 
 			this.elem.addEventListener('mousedown', (e) => {
 				if (timeOfClick + waitBeforeDrag > Date.now()) return;
@@ -99,26 +90,24 @@ if (bigCookie) {
 				this.attractTowards(e.x, e.y);
 				document.body.style.userSelect = 'none';
 				document.body.style.pointerEvents = 'none';
-				this.elem.style.cursor = 'grabbing';
 			});
 
 			this.elem.style.pointerEvents = 'none';
 			setTimeout(() => (this.elem.style.pointerEvents = 'all'), waitBeforeDrag);
 
-			this.elem.style.setProperty('--left', `${this.pos.x}px`);
-			this.elem.style.setProperty('--top', `${this.pos.y}px`);
-			this.elem.style.setProperty('--size', `${this.size}px`);
-			this.elem.style.setProperty('--rotation', `${this.rotation}deg`);
+			this.elem.style.left = `${this.pos.x}px`;
+			this.elem.style.top = `${this.pos.y}px`;
+			this.elem.style.width = `${this.size}px`;
+			this.elem.style.opacity = String(this.opacity);
 		}
 		attractTowards(x, y) {
 			this.vel.x = x - (this.pos.x + this.size / 2);
 			this.vel.y = y - (this.pos.y + this.size / 2);
 		}
 		update() {
-			if (this.isDead) return;
+			if (this.opacity < 0) return;
 			this.pos.x += this.vel.x;
 			this.pos.y += this.vel.y;
-			this.rotation += this.vel.x * 0.2;
 
 			// Constrain from screen edges
 			if (this.pos.x < 0) {
@@ -137,27 +126,36 @@ if (bigCookie) {
 				this.vel.y = (-this.vel.y / 4) * 3; // Dampen bouncing
 			}
 
-			// Return opacity to normal
-			if (draggedBit === this && this.opacity < 1) {
-				this.opacity += 0.01;
-			}
-			// when on floor
-			else if (this.pos.y + this.size >= innerHeight) {
+			const onCeil = this.pos.y <= 0;
+			const onFloor = this.pos.y + this.size >= innerHeight;
+			const onLeft = this.pos.x <= 0;
+			const onRight = this.pos.x + this.size >= innerWidth;
+			if (draggedBit === this) {
+				// Return opacity to normal when dragging
+				if (this.opacity < 1) this.opacity += 0.1;
+
+				// Rotate on edges, only when one edge is touching
+				if (onCeil + onFloor + onLeft + onRight === 1) {
+					const vel = onFloor || onCeil ? this.vel.x : this.vel.y;
+					const dir = onFloor || onLeft ? 1 : -1;
+					this.rotation += vel * 0.5 * dir;
+				}
+			} else if (onFloor) {
 				this.vel.x = this.vel.x * 0.9; // Dampen speed
-				this.rotation += this.vel.x * 0.5; // Rotate faster
+				this.rotation += this.vel.x * 0.5; // Fake floor rolling
 				this.opacity -= 0.004; // Lower opacity
+			} else {
+				// When flying through air
+				this.rotation += this.vel.x * 0.2;
 			}
 
 			// Add gravity last so that constraints don't affect it
 			this.vel.y += 1;
 
-			this.elem.style.setProperty('--left', `${this.pos.x}px`);
-			this.elem.style.setProperty('--top', `${this.pos.y}px`);
-			this.elem.style.setProperty('--rotation', `${this.rotation}deg`);
-			this.elem.style.setProperty('--opacity', String(this.opacity));
-
-			this.isDead = this.opacity <= 0;
-			if (this.isDead) this.elem.remove();
+			this.elem.style.left = `${this.pos.x}px`;
+			this.elem.style.top = `${this.pos.y}px`;
+			this.elem.style.rotate = `${this.rotation}deg`;
+			this.elem.style.opacity = String(this.opacity);
 		}
 	}
 
@@ -172,39 +170,49 @@ if (bigCookie) {
 				y: dir[1] * randomFloat(0.3, 3),
 			};
 			this.opacity = 1;
-			this.elem.style.setProperty(
-				'--offset-left',
-				`${randomFloat(-this.size / 2, this.size / 2)}px`,
-			);
-			this.elem.style.setProperty(
-				'--offset-top',
-				`${randomFloat(-this.size / 2, this.size / 2)}px`,
-			);
-			this.elem.style.setProperty('--left', `${this.pos.x}px`);
-			this.elem.style.setProperty('--top', `${this.pos.y}px`);
-			this.elem.style.setProperty('--size', `${this.size}px`);
+			const offset = {
+				x: randomFloat(-this.size / 2, this.size / 2),
+				y: randomFloat(-this.size / 2, this.size / 2),
+			};
+			this.elem.style.backgroundPosition = `${offset.x}px ${offset.y}px`;
+			this.elem.style.left = `${this.pos.x}px`;
+			this.elem.style.top = `${this.pos.y}px`;
+			this.elem.style.width = `${this.size}px`;
 		}
 		update() {
-			if (this.isDead) return;
+			if (this.opacity < 0) return;
 			this.opacity -= 0.04;
 			this.pos.x += this.vel.x;
 			this.pos.y += this.vel.y;
 			this.vel.y += 0.2; // Gravity
 
 			this.elem.style.opacity = this.opacity;
-			this.elem.style.setProperty('--left', `${this.pos.x}px`);
-			this.elem.style.setProperty('--top', `${this.pos.y}px`);
-			this.elem.style.setProperty('--size', `${this.size}px`);
-
-			this.isDead = this.opacity <= 0 || this.pos.y < 0;
-			if (this.isDead) this.elem.remove();
+			this.elem.style.left = `${this.pos.x}px`;
+			this.elem.style.top = `${this.pos.y}px`;
 		}
+	}
+
+	function createCrack(e) {
+		const bigCookiePos = bigCookie.getBoundingClientRect();
+		const crack = {
+			opacity: 1,
+			img: document.createElement('img'),
+		};
+		const crackSize = 30;
+		crack.img.src = 'assets/crack.png';
+		crack.img.classList.add('cookieCrack');
+		crack.img.style.width = `${crackSize}px`;
+		crack.img.style.left = `${e.pageX - crackSize / 2 - bigCookiePos.x}px`;
+		crack.img.style.top = `${e.pageY - crackSize / 2 - bigCookiePos.y}px`;
+		crack.img.style.rotate = `${randomFloat(0, 360)}deg`;
+		return crack;
 	}
 
 	(function loop() {
 		for (let i = 0; i < cookieBits.length; i++) {
 			cookieBits[i].update();
-			if (cookieBits[i].isDead) {
+			if (cookieBits[i].opacity < 0) {
+				cookieBits[i].elem.remove();
 				const last = cookieBits.length - 1;
 				[cookieBits[i--], cookieBits[last]] = [cookieBits[last], cookieBits[i]];
 				if (cookieBits.pop() === draggedBit) draggedBit = undefined;
@@ -214,8 +222,7 @@ if (bigCookie) {
 			const crack = cracks[i];
 			crack.opacity -= 0.02;
 			crack.img.style.opacity = crack.opacity;
-			const isDead = crack.opacity < 0;
-			if (isDead) {
+			if (crack.opacity < 0) {
 				crack.img.remove();
 				const last = cracks.length - 1;
 				[cracks[i--], cracks[last]] = [cracks[last], cracks[i]];
